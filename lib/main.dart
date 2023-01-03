49780edae041ada3878bf7fd8be2f00e596a4534
toshiossada <lucascostabustamante@gmail.com>
2023-01-03 14:17:41 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/colors.dart';
-import 'package:portfolio/view/home.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:portfolio/resource/colors.dart';
+import 'package:portfolio/view/root.dart';
 
-void main(){
-  runApp(AnimatedBuilder(
-    animation: Controller.instance,
-      builder: (context, child){
-      return MaterialApp(
-        home: HomeBackGround(),
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          accentColor: kPrimaryColor,
-          brightness: Controller.instance.onOff ?
-              Brightness.dark : Brightness.light
-        ),
-      );
-      },
-  ));
+void main() {
+  runApp(ProviderScope(child: const AppTheme()));
 }
 
+class AppTheme extends StatelessWidget {
+  const AppTheme({Key? key}) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: Controller.instance,
+        builder: (context, child){
+      return MaterialApp(
+          home: RootScreen(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+    accentColor: kPrimaryColor,
+    brightness: Controller.instance.onOff ?
+    Brightness.dark : Brightness.light
+    ));
+  }
+  );
+  }
+}
