cc0ebd7c1d3d0ae5ecb4f3e9f077596b3965560f
toshiossada <lucascostabustamante@gmail.com>
2023-01-03 14:25:22 -0300

    rightPane

---
+++ lib/view/widget/rightPane.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../../resource/colors.dart';
+
+class RightPane extends StatefulWidget {
+  const RightPane({Key? key}) : super(key: key);
+
+  @override
+  State<RightPane> createState() => _RightPaneState();
+}
+
+class _RightPaneState extends State<RightPane> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 70),
+      child: Expanded(
+          flex: 1,
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Expanded(
+                    flex: 5,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 25),
+                          child: RotatedBox(
+                              quarterTurns: 1,
+                              child: Text(
+                                'Site criado usando Flutter',
+                                style: TextStyle(
+                                    letterSpacing: 1,
+                                    color: textColor,
+                                    fontSize: 14,
+                                    fontFamily: 'sfmono'),
+                              )),
+                        )
+                      ],
+                    )),
+                Expanded(
+                    child: Container(
+                  width: 1,
+                  color: textColor,
+                ))
+              ],
+            ),
+          )),
+    );
+  }
+}
