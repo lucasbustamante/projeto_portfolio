58c7754edec0422c90eb42a1d5374a270b87f953
toshiossada <lucascostabustamante@gmail.com>
2023-01-04 16:18:07 -0300

    introTab

---
+++ lib/view/intro/introTab.dart
@@ -0,0 +1,132 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/resource/strings.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
+import '../../resource/appClass.dart';
+import '../../resource/colors.dart';
+
+class IntroTab extends StatefulWidget {
+  AutoScrollController aScrollController;
+
+  IntroTab(this.aScrollController, {Key? key}) : super(key: key);
+
+  @override
+  State<IntroTab> createState() => _IntroTabState();
+}
+
+class _IntroTabState extends State<IntroTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.transparent,
+      margin: EdgeInsets.only(
+          left: AppClass().getMqWidth(context) * 0.01,
+          top: AppClass().getMqHeight(context) * 0.07),
+      padding: EdgeInsets.only(bottom: 50),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  Strings.welcomeTxt,
+                  style: TextStyle(
+                      color: primaryColor,
+                      fontSize: 16,
+                      fontFamily: 'sfmono'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  Strings.name,
+                  style: GoogleFonts.robotoSlab(
+                    color: primaryColor,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 3,
+                    fontSize: 45,
+                  ),
+                ),
+              ),
+              Container(
+                width: AppClass().getMqWidth(context) -
+                    (AppClass().getMqWidth(context) * 0.23),
+                child: AnimatedTextKit(
+                  repeatForever: true,
+                  pause: const Duration(seconds: 5),
+                  animatedTexts: [
+                    TyperAnimatedText(
+                        Strings.whatIdo,
+                        textAlign: TextAlign.start,
+                        textStyle: GoogleFonts.robotoSlab(
+                          color: textColor,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 3,
+                          fontSize: 45,)),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Container(
+                  width: AppClass().getMqWidth(context) * 0.45,
+                  child: RichText(
+                      text: TextSpan(
+                          text: Strings.introAbout,
+                          style: GoogleFonts.roboto(
+                            color: textLight,
+                            letterSpacing: 1,
+                            height: 1.5,
+                            fontSize: 16,
+                          ),
+                          children: <TextSpan>[
+                        TextSpan(
+                          text: Strings.variantName,
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.bold,
+                            color: primaryColor,
+                            letterSpacing: 1,
+                            height: 1.5,
+                            fontSize: 16,
+                          ),
+                        )
+                      ])),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0, bottom: 50),
+                child: InkWell(
+                  onTap: () {
+                    widget.aScrollController.scrollToIndex(1,
+                        preferPosition: AutoScrollPosition.begin);
+                  },
+                  child: Container(
+                    height: AppClass().getMqHeight(context) * 0.09,
+                    width: AppClass().getMqWidth(context) * 0.25,
+                    decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                        border: Border.all(
+                            color: primaryColor, width: 1.5)),
+                    child: Center(
+                      child: Text('Vamos lá!',
+                          style: TextStyle(
+                              color: primaryColor,
+                              fontSize: 13,
+                              letterSpacing: 1,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'sfmono')),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
